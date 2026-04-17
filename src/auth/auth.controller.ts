882f52adc99e4987780415e88ba2f52df26a0754
stsTom <tamilaustsiapan@gmail.com>
2026-04-17 07:49:31 +0200

    Set up token refresh endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,10 +3,14 @@ import {
   Controller,
   HttpCode,
   HttpStatus,
-  Post
+  Post,
+  UnauthorizedException,
+  UsePipes,
+  ValidationPipe
 } from '@nestjs/common'
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { RefreshDto } from './dto/refresh.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -22,5 +26,16 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   login(@Body() authDto: AuthDto) {
     return this.authService.login(authDto)
+  }
+
+  @Post('refresh')
+  @UsePipes(new ValidationPipe ({
+    exceptionFactory(errors) {
+      new UnauthorizedException(`No refresh token`)
+    },
+  }))
+  @HttpCode(HttpStatus.OK)
+  refresh(@Body() refreshDto: RefreshDto) {
+    return this.authService.refreshTokens(refreshDto)
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { UsersService } from "@/users/users.service";
 import { BadRequestException, ForbiddenException, Inject, Injectable } from "@nestjs/common";
 import { AuthDto } from "./dto/auth.dto";
 import { JwtService } from "@nestjs/jwt";
+import { RefreshDto } from "./dto/refresh.dto";
+import { verify } from "jsonwebtoken";
 
 @Injectable()
 export class AuthService {
@@ -28,9 +30,27 @@ export class AuthService {
       throw new ForbiddenException(`${dto.login} doesn't exist or password is invalid`)
     }
 
-    const accessToken = this.accessTokenService.sign(user)
-    const refreshToken = this.refreshTokenService.sign(user)
+    const payload = { sub: user.id }
+    const tokens = await this.createTokens(payload)
     
+    return tokens
+  }
+  
+  async refreshTokens(dto: RefreshDto){
+    try{
+      const valid = verify(dto.refreshToken, process.env.JWT_SECRET_REFRESH_KEY)
+      const newTokens = await this.createTokens(valid.sub)
+      
+      return newTokens
+    }catch(error){
+      throw new ForbiddenException(`Authentication failed: refresh token is invalid or it has expired`)
+    }
+  }
+
+  async createTokens(payload){
+    const accessToken = this.accessTokenService.sign(payload)
+    const refreshToken = this.refreshTokenService.sign(payload)
+
     return { accessToken, refreshToken }
   }
 }
